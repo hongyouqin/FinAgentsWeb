af16c69f65f47e649cda7dea7aedb0624dfdcba7
luolichuan <18230851423@163.com>
2026-04-16 16:08:13 +0800

    问题修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -536,7 +536,7 @@ router.beforeEach(async (to, from, next) => {
   // 设置页面标题
   const title = to.meta.title as string
   if (title) {
-    document.title = `${title} - 牛逼股票`
+    document.title = `${title} - tradingagents`
   }
 
   console.log('🚦 路由守卫检查:', {
